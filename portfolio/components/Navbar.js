0c4e3d7f03751ad04540d500cf488f169f7903e2
Sahil Shetty <70585867+ACENSONIX@users.noreply.github.com>
2022-07-12 06:28:02 +0530

    added Navbar and sidebar

---
+++ portfolio/components/Navbar.js
@@ -0,0 +1,31 @@
+import { Flex, Box } from "@chakra-ui/react";
+import styles from "../styles/Home.module.css";
+
+const Navbar = () => {
+  return (
+    <>
+      <Box className={styles.navbar}>
+        <Flex
+          justify="flex-start"
+          align="center"
+          paddingLeft={4}
+          paddingTop={20}
+        >
+          <Box className={styles.name}>SAHIL SHETTY</Box>
+          <Box className={styles.links}>
+            <Flex justify="space-around">
+              <a className={styles.navlinks} href="/about">
+                About
+              </a>
+              <a href="/education">Education</a>
+              <a href="/projects">Projects</a>
+              <a href="/contact">Contact</a>
+            </Flex>
+          </Box>
+        </Flex>
+      </Box>
+    </>
+  );
+};
+
+export default Navbar;
