8a5f640ce89285b340406563d2dbdca2e0572963
Sahil Shetty <70585867+ACENSONIX@users.noreply.github.com>
2022-07-14 16:20:41 +0530

    added hero section

---
+++ portfolio/components/Sections/Hero.js
@@ -0,0 +1,28 @@
+import styles from "../../styles/Home.module.css";
+import { Flex, Box, Button } from "@chakra-ui/react";
+
+const Hero = () => {
+  return (
+    <>
+      <Box className={styles.hero}>
+        <h1 className={styles.smhead}>Hi, my name is</h1>
+        <h2 className={styles.name}>Sahil Shetty</h2>
+        <h3 className={styles.bigheading}>I build things for the web.</h3>
+        <p className={styles.desc}>
+          I'm a second year I.T student at D.J Sanghavi College of Engineering.
+          <br></br>I'm a <b>MERN developer </b>with a passion for learning new
+          things and
+          <br></br> building things that are useful to the world. I like problem
+          solving and<br></br> love solving challenges. Currently looking for
+          <b> Web Development opportunities.</b>
+        </p>
+
+        <Button size="md" className={styles.rsbtn}>
+          Resume
+        </Button>
+      </Box>
+    </>
+  );
+};
+
+export default Hero;
--- portfolio/pages/index.js
+++ portfolio/pages/index.js
@@ -1,14 +1,14 @@
-import Head from "next/head";
-import Image from "next/image";
 import Navbar from "../components/Navbar";
+import Hero from "../components/Sections/Hero";
 import Side from "../components/Side";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Navbar />
-      <Side />
+      {/* <Navbar />
+      <Side /> */}
+      <Hero />
     </div>
   );
 }
